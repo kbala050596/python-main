2da69d9fd1dba9f17e68b42718d3bd92902c0287
kbala050596 <kbala16609@gmail.com>
2023-09-03 02:41:59 +0000

    learning git revert

--- first-code.py
+++ first-code.py
@@ -1,3 +1,3 @@
-m = {'a': 12, 'b': 21, 'c': 31, 'd': 4} 
+m = {'a': 1243, 'b': 2421, 'c': 3122, 'd': 4222} 
 for key, value in m.items():
     print('{0}: {1}'.format(key, value))
--- second-code.sh
+++ second-code.sh
@@ -3,5 +3,10 @@
 a=56
 b=76
 c=86
-sum=a+b+c
+
+a=56112
+b=76113
+c=86212
+d=9219
+sum=a+b+c+d
 echo sum
